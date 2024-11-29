331862db7e1f4b6183f4b8ef3d2fce90c3df6b89
palascinochristian <palascinochristian@gmail.com>
2024-11-29 13:11:01 +0100

    fixed comment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for(let i=1; i<=100 ; i++){
     //Se il numero è un multiplo di 5 scrivo Buzz
     } else if (i % 5 == 0){
         console.log("Buzz")
-    //Se il numero è un multiplo di entrambi scrivo FizzBuzz
+    //Se il numero è un multiplo di 3 scrivo FizzBuzz
     } else if (i % 3 == 0 ){
         console.log("Fizz")
     } else {
