01e38ca16c095acc6f8f34169ff3cddc3ab0df55
palascinochristian <palascinochristian@gmail.com>
2024-11-29 12:59:32 +0100

    end project

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scrivo un programma che stampi numeri da 1 a 100
+
+for(let i=1; i<=100 ; i++){
+    //Se il numero è un multiplo di 3 scrivo Fizz
+    if(i % 3 == 0){
+        console.log("Fizz")
+    //Se il numero è un multiplo di 5 scrivo Buzz
+    } else if (i % 5 == 0){
+        console.log("Buzz")
+    //Se il numero è un multiplo di entrambi scrivo FizzBuzz
+    } else if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    } else {
+        console.log(i)
+    }
+}
